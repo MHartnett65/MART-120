fe2ff3eb7bcebcdf96fa41a62d0c49a3d6488d12
MHartnett65 <Hartnett6590@icloud.com>
2023-05-01 20:24:20 -0600

    Week 15

---
+++ Homework 15/sketch.js
@@ -0,0 +1,152 @@
+var characterX = 100;
+var characterY = 100;
+
+var w = 87;
+var s = 83;
+var a = 65;
+var d = 68;
+
+
+var shapeX = 30;
+var shapeY = 50;
+
+var shapeXs = [];
+var shapeYs = [];
+var diameters = [];
+
+var shapeXSpeeds = [];
+var shapeYSpeeds = [];
+
+
+var mouseShapeX;
+var mouseShapeY;
+
+var rectangleObject;
+var shape1
+var shape2
+function setup() {
+    createCanvas(500, 600);
+    
+    rectangleObject = new Rectangle(100, 200, 350, 50, 120, 35, 210);
+    shape1 = new Rectangle(50, 250, 350, 50, 135, 25, 200);
+    shape2 = new Rectangle(250, 50, 10, 45, 170, 45, 250)
+    for (var i = 0; i < 50; i++) {
+        shapeXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+        shapeYSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+        shapeXs[i] = getRandomNumber(500);
+        shapeYs[i] = getRandomNumber(600);
+        diameters[i] = getRandomNumber(30);
+    }
+
+    createCharacter(200, 350);
+}
+
+function draw() {
+    background(190, 90, 85);
+    stroke(0);
+    rectangleObject.display();
+    shape1.display();
+    shape2.display();
+    fill(0);
+
+    createBorders(20);
+
+    textSize(16);
+    text("RUN!!!", width - 50, height - 50)
+
+    drawCharacter();
+    characterMovement();
+
+
+    
+    fill(20, 200, 50);
+    circle(shapeX, shapeY, 30);
+
+    circle(shapeY, shapeX, 40)
+    fill(13,150,14);
+    circle(shapeX, shapeY, 10);
+
+    for (var i = 0; i < shapeXs.length; i++) {
+        circle(shapeXs[i], shapeYs[i], diameters[i]);
+        shapeXSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+        shapeYSpeeds[i] = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
+
+
+
+
+        
+        shapeXs[i] += shapeXSpeeds[i];
+        shapeYs[i] += shapeYSpeeds[i];
+        
+        if (shapeXs[i] > width) {
+            shapeXs[i] = 0;
+        }
+        if (shapeXs[i] < 0) {
+            shapeXs[i] = width;
+        }
+        if (shapeYs[i] > height) {
+            shapeYs[i] = 0;
+        }
+        if (shapeYs[i] < 0) {
+            shapeYs[i] = height;
+        }
+    }
+    
+    if (characterX > width && characterY > width - 50) {
+        fill(0);
+        stroke(5);
+        textSize(26);
+        text("Victory!", width / 2 - 50, height / 2 - 50);
+    }
+
+    
+    fill(120, 130, 140);
+    circle(mouseShapeX, mouseShapeY, 25);
+}
+
+function characterMovement() {
+    
+    if (keyIsDown(w)) {
+        characterY -= 10;
+    }
+    if (keyIsDown(s)) {
+        characterY += 10;
+    }
+    if (keyIsDown(a)) {
+        characterX -= 10;
+        console.log("movement: " + characterX);
+    }
+    if (keyIsDown(d)) {
+        characterX += 10;
+    }
+}
+
+function createCharacter(x, y) {
+    characterX = x;
+    characterY = y;
+}
+
+function drawCharacter() {
+    fill(110, 100, 250);
+    circle(characterX, characterY, 25);
+}
+
+function createBorders(thickness) {
+    
+    rect(0, 0, width, thickness);
+    
+    rect(0, 0, thickness, height);
+    
+    rect(0, height - thickness, width, thickness);
+    
+    rect(width - thickness, 0, thickness, height - 50);
+}
+
+function mouseClicked() {
+    mouseShapeX = mouseX;
+    mouseShapeY = mouseY;
+}
+
+function getRandomNumber(number) {
+    return Math.floor(Math.random() * number) + 10;
+}
